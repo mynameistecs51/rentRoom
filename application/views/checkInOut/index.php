089c559fa67324072d1a81c477ede2e979fe4647
chaiwat <mynameistecs51@gmail.com>
2017-06-09 22:40:39 +0700

    อัพเดท index.php

--- application/views/checkInOut/index.php
+++ application/views/checkInOut/index.php
@@ -23,14 +23,14 @@
 
 	<div class="panel-body " >
 		<div class="row">
-		<div class="col-sm-12 alert-warning text-center"><h3>ชั้น 1</h3></div>
+			<div class="col-sm-12 alert-warning text-center"><h3>ชั้น 1</h3></div>
 			<div class=" col-sm-12 alert-info"  style="padding:20px;">
 				<?php	for ($i = 1; $i <= 24; ++$i) :?>
-					<div class="col-sm-1 ">
+					<div class="col-sm-2 ">
 						<?php echo  $sho = ($i == 5 )?
-						'<button  class="btn btn-danger col-sm-12 btn_room"  style="margin:10px;" ><i class="fa fa-bed fa-lg" aria-hidden="true"></i><h5>A'.$i.'  <i class="fa fa-square" aria-hidden="true" style="background-color:orange;color:orange;"></i></h5></button>'
+						'<a  tabindex="0"  class="btn btn-danger col-sm-12 btn_room"  role="button"  data-num_room="'.$i.'"  style="margin:10px;" ><i class="fa fa-bed fa-lg" aria-hidden="true"></i><h5>A'.$i.'  <i class="fa fa-square" aria-hidden="true" style="background-color:orange;color:orange;"></i></h5></a>'
 						:
-						'<button  class="btn btn-default col-sm-12 btn_room"   style="margin:10px;"><i class="fa fa-bed fa-lg " aria-hidden="true"></i><h5>A '.$i.'</h5></button>';?>
+						'<a  tabindex="0"  class="btn btn-default col-sm-12 btn_room"  role="button"  data-num_room="'.$i.'"    style="margin:10px;"><i class="fa fa-bed fa-lg " aria-hidden="true"></i><h5>A '.$i.'</h5></a>';?>
 					</div>
 				<?php endfor?>
 			</div>
@@ -41,19 +41,33 @@
 <?php echo $footer; ?>
 <script type="text/javascript">
 	$(function () {
-		$('.btn_room').popover({
-			'trigger': 'focus ',
-			'placement':'right',
-			'title': ' ทำรายการ<span class=" glyphicon glyphicon-remove pull-right"></span>',
-			'content': '<div class="col-sm-12">'+
-			'<button class="btn btn-danger col-sm-12  btn-sm" style="margin-top:5px;"> CheckIn</button>'+
-			'<button class="btn btn-default col-sm-12 btn-sm" style="margin-top:5px;"> CheckOut</button>'+
-			'<button class="btn btn-success col-sm-12 btn-sm" style="margin-top:5px;"> Clean</button>'+
-			'<button class="btn btn-warning col-sm-12 btn-sm" style="margin-top:5px;"> จอง</button>'+
-			'</div>',
-			'html':true
-		});
+		getRoom();
+
 	});
+
+	function getRoom() {
+		$('.btn_room').on('click',function(){
+			var num_room = $('.btn_room').data('num_room');
+			$(this).popover({
+				'trigger': 'focus',
+				'placement':'right',
+				'title': ' ทำรายการ<span class=" glyphicon glyphicon-remove pull-right"></span>',
+				'content': '<div class="col-sm-12 popover_content">'+
+				'<button  class="btn btn-danger col-sm-12  btn-sm btn_checkin" style="margin-top:5px;" data-num_room="'+num_room+'"> CheckIn</button>'+
+				'<button  class="btn btn-danger col-sm-12  btn-sm btn_checkout" style="margin-top:5px;" data-num_room="'+num_room+'"> CheckOut</button>'+
+				'<button  class="btn btn-success col-sm-12 btn-sm btn_clean" style="margin-top:5px;"  data-num_room="'+num_room+'"> Clean</button>'+
+				'<button  class="btn btn-warning col-sm-12 btn-sm btn_reservation" style="margin-top:5px;"  data-num_room="'+num_room+'"> จอง</button>'+
+				'</div>',
+				'html':true,
+			}).popover('toggle'),
+			$('.btn_checkin').on('click', function() {
+				alert('ห้อง A'+$(this).data('num_room'));
+			});
+			$('.btn_checkout').on('click', function() {
+				alert('ห้อง A'+$(this).data('num_room'));
+			});
+		});
+	}
 </script>
 
 
