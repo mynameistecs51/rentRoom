02106580e90a6f4f85bc7372ad5125781c911c1f
chaiwat <mynameistecs51@gmail.com>
2017-06-09 09:07:56 +0700

    update index.php

--- application/views/checkInOut/index.php
+++ application/views/checkInOut/index.php
@@ -4,6 +4,15 @@
 	<div class="panel-heading"><i class="glyphicon glyphicon-list"></i> บันทึกการเข้าพัก</div>
 	<div class="navbar navbar-default" style="padding-top: 5px;">
 		<div class="form-inline">
+			<div class="form-group col-sm-6">
+				<i class="fa fa-square-o" aria-hidden="true" style="background-color:gray;color:gray;"></i> = ว่าง,
+				<i class="fa fa-square text-danger" aria-hidden="true" ></i> = อยู่,
+				<i class="fa fa-square text-success" aria-hidden="true" ></i> = ทำความสะอาด,
+				<i class="fa fa-square text-warning" aria-hidden="true" ></i> = จอง
+				<div class="text-primary line-height">
+					<u> วันที่ <?php echo date('j').' เดือน '.$getMonth[$mounth].' พ.ศ. '.$getYear[date('Y')];?> </u>
+				</div>
+			</div>
 			<div class="form-group pull-right">
 				<label for="search">ค้นหา: </label>
 				<input type="text" class="form-control" id="search" placeholder="-- หมายเลขห้อง --">
@@ -12,105 +21,20 @@
 		</div>
 	</div>
 
-	<div class="panel-body ">
+	<div class="panel-body " >
 		<div class="row">
-			<div class="col-sm-12" style="padding-bottom: 3px;">
-				<div class=" col-sm-6">
-					<i class="fa fa-square-o" aria-hidden="true" ></i> = ว่าง,
-					<i class="fa fa-square-o" aria-hidden="true" style="background-color:red;color:red;"></i> = อยู่,
-					<i class="fa fa-square-o" aria-hidden="true" style="background-color:green;color:green;"></i> = ทำความสะอาด
-					<div class="text-primary line-height">
-						<u> วันที่ <?php echo date('j').' เดือน '.$getMonth[$mounth].' พ.ศ. '.$getYear[date('Y')];?> </u>
+			<div class=" col-sm-12 alert-info"  style="padding:20px;">
+				<?php	for ($i = 1; $i <= 24; ++$i) :?>
+					<div class="col-sm-1 ">
+						<?php echo  $sho = ($i == 5 )?
+						'<button class="btn btn-danger col-sm-12" style="margin:10px;" ><i class="fa fa-bed fa-lg" aria-hidden="true"></i><h5>A'.$i.'  <i class="fa fa-square text-warning" aria-hidden="true" ></i></h5></button>'
+						:
+						'<button class="btn btn-default col-sm-12 " style="margin:10px;"><i class="fa fa-bed fa-lg " aria-hidden="true"></i><h5>A '.$i.'</h5></button>';?>
 					</div>
-				</div>
-				<div class=" col-sm-6" >
-					<!-- <form class="form-inline" method="post" action="<?php //echo base_url('checkInOut'); ?>" > -->
-					<?php echo form_open('checkInOut', 'class=" col-sm-12" '); ?>
-					<div class="form-group col-sm-5">
-						<select name="mounth" class ="form-control" >
-							<option value="">  เดือน </option>
-							<?php foreach ($getMonth as $keyMounth => $mounth): ?>
-								<option  value="<?php echo $keyMounth; ?>">
-									<?php echo $mounth; ?>
-								</option>
-							<?php endforeach ?>
-						</select>
-					</div>
-					<div class="form-group col-sm-5">
-						<select name="year" class ="form-control" >
-							<option value="">  พ.ศ  </option>
-							<?php foreach ($getYear as $keyYear => $Year): ?>
-								<option  value="<?php echo $keyYear; ?>"> <?php echo $Year; ?> </option>
-							<?php endforeach ?>
-						</select>
-					</div>
-					<button type="submit" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-search"></span></button>
-					<?php echo form_close(); ?>
-				</div>
+				<?php endfor?>
 			</div>
-
-			<table class="table table-hover table-bordered col-sm-12 "  style="width: 100%;overflow-y: auto;overflow-x: auto;display: block;" >
-				<thead border="1"  >
-					<tr class="alert-info" >
-						<th style="text-align: center;padding: 3px;" > ห้อง</th>
-						<th style="text-align: center;padding: 3px;" > ราคา</th>
-						<?php for ($i=$startDay; $i <= $lastDay; $i++) :?>
-							<th style="text-align: center;padding:3px;"> <span style="font-size: 9px;">วันที่</span><br><?php echo $i; ?></th>
-						<?php endfor ?>
-					</tr>
-				</thead>
-				<tbody>
-					<tr >
-						<td style="text-align: center;" class="alert-default">101</td>
-						<td style="text-align: center;">550</td>
-						<?php for ($i=$startDay; $i <= $lastDay; $i++) :?>
-							<td style="text-align: center;padding: 3px;" >
-								<button class="btn btn-default btn-xs btn_dateRoom" data-numRoom='<?php echo $i ?>'><i class="fa fa-bed" aria-hidden="true"></i></button>
-							</td>
-						<?php endfor; ?>
-					</tr>
-					<tr >
-						<td style="text-align: center;" class="alert-default">201</td>
-						<td style="text-align: center;">250</td>
-						<?php for ($i=$startDay; $i <= $lastDay; $i++) :?>
-							<td style="text-align: center;padding: 3px;" >
-								<button class="btn btn-default btn-xs btn_dateRoom" data-numRoom='<?php echo $i ?>'><i class="fa fa-bed" aria-hidden="true"></i></button>
-							</td>
-						<?php endfor; ?>
-					</tr>
-					<tr >
-						<td style="text-align: center;" class="alert-default">301</td>
-						<td style="text-align: center;">250</td>
-						<?php for ($i=$startDay; $i <= $lastDay; $i++) :?>
-							<td style="text-align: center;padding: 3px;" >
-								<button class="btn btn-default btn-xs btn_dateRoom" data-numRoom='<?php echo $i ?>'><i class="fa fa-bed" aria-hidden="true"></i></button>
-							</td>
-						<?php endfor; ?>
-					</tr>
-				</tbody>
-			</table>
 		</div>
 	</div>
 </div>
-<?php echo $footer; ?>
 
-<script type="text/javascript">
-	$(function(){
-		$('.btn_dateRoom').click(function(){
-			$("<div title='บันทึกรายการ'>" + "ห้อง 101 วันที"+$(this).data('numroom')+"</div>").dialog({
-				modal: true,
-				buttons: {
-					'ok': {
-						'text': 'OK',
-						'prepend': '<b class="glyphicon glyphicon-ok"></b> ',
-						'class': 'btn btn-primary',
-						'click': function(){
-							$(this).dialog('close');
-						}
-					}
-				}
-			});
-		});
-	});
-</script>
 
