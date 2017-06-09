d768f9d22cce006d4a97ccb448759987af06706a
Dittaphong Nilnama <aostudio.nilnama@gmail.com>
2017-06-09 10:35:15 +0700

    DB - Design เพิ่มเอกสาร ออกแบบฐานข้อมูล

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('Template','calendar');
+$autoload['libraries'] = array('database', 'Template','calendar');
 
 /*
 | -------------------------------------------------------------------
