c0e2c345f60d686c882cb6ac24a936997fc66ef7
chaiwat <mynameistecs51@gmail.com>
2017-06-12 11:34:13 +0700

    update checkout & report

--- application/views/checkOut/index.php
+++ application/views/checkOut/index.php
@@ -1,29 +1,31 @@
 <?php echo $header; ?>
-<table id="example" class="display" cellspacing="0" width="100%">
-	<thead>
-		<tr>
-			<th>ชื่อ - สกุล</th>
-			<th>ห้อง</th>
-			<th>รายการ</th>
-			<th>จัดการ</th>
+<div class="container">
+	<table id="example" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%"  >
+		<thead style="background: #e6e6e6;">
+			<tr>
+				<th>ชื่อ - สกุล</th>
+				<th>ห้อง</th>
+				<th>รายการ</th>
+				<th>จัดการ</th>
 
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<th>นาย ไชยวัฒน์ หอมแสง</th>
-			<td>ห้อง 202</td>
-			<td>ห้องเตียงคู่</td>
-			<td><button class="btn btn-primary">Check Out</button></td>
-		</tr>
-		<tr>
-			<th>นาย ไชยวัฒน์ หอมแสง</th>
-			<td>ห้อง 203</td>
-			<td>ห้องเตียงเดี่ยว</td>
-			<td><button class="btn btn-primary">Check Out</button></td>
-		</tr>
-	</tbody>
-</table>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>นาย ไชยวัฒน์ หอมแสง</th>
+				<td>ห้อง 202</td>
+				<td>ห้องเตียงคู่</td>
+				<td><button class="btn btn-primary">Check Out</button></td>
+			</tr>
+			<tr>
+				<th>นาย ไชยวัฒน์ หอมแสง</th>
+				<td>ห้อง 203</td>
+				<td>ห้องเตียงเดี่ยว</td>
+				<td><button class="btn btn-primary">Check Out</button></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 <?php echo $footer; ?>
 
 <script type="text/javascript">
@@ -31,5 +33,5 @@
 		$('#example').DataTable();
 	});
 
-	window.print();
+	// window.print($('#example').data);
 </script>
--- application/views/report/index.php
+++ application/views/report/index.php
@@ -1,39 +1,55 @@
 <?php echo $header; ?>
-<div class="col-sm-12 alert-info">
+<div class="col-sm-12 ">
 	<div class="col-sm-12 text-center">จันเจ้าโฮเทล (JanJao Hotel)</div>
-	<div class="col-sm-12 alert-info">
-		โรงแรมจันทร์เจ้า <br>
-		666 หมู่ 6 ถ.โพนพิสัย ต.หมากแข้ง <br>
-		อ.เมือง จ.อุดรธานี 41000 <br>
-		โทร 0910571616
-		<table width="100%" bgcolor="1">
-			<thead>
-				<tr>
-					<th>ชื่อ - สกุล</th>
-					<th>ห้อง</th>
-					<th>รายการ</th>
-					<th>จัดการ</th>
+	<div class="col-sm-12 ">
 
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<th>นาย ไชยวัฒน์ หอมแสง</th>
-					<td>ห้อง 202</td>
-					<td>ห้องเตียงคู่</td>
-					<td> 500 Bath.</button></td>
-				</tr>
-				<tr>
-					<th>นาย ไชยวัฒน์ หอมแสง</th>
-					<td>ห้อง 203</td>
-					<td>ห้องเตียงเดี่ยว</td>
-					<td>500 Bath.</td>
-				</tr>
-			</tbody>
-		</table>
+		<table width="100%" border="1" id="example">
+			<caption>โรงแรมจันทร์เจ้า 666 หมู่ 6
+				ถ.โพนพิสัย ต.หมากแข้ง <br>
+				อ.เมือง จ.อุดรธานี 41000 <br>
+				โทร 0910571616</caption>
+				<thead class="bg-warning">
+					<tr>
+						<th>ชื่อ - สกุล</th>
+						<th>ห้อง</th>
+						<th>รายการ</th>
+						<th>จัดการ</th>
+
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>นาย ไชยวัฒน์ หอมแสง</td>
+						<td>ห้อง 202</td>
+						<td>ห้องเตียงคู่</td>
+						<td>500 Bath.</td>
+					</tr>
+					<tr>
+						<td>นาย ไชยวัฒน์ หอมแสง</td>
+						<td>ห้อง 203</td>
+						<td>ห้องเตียงเดี่ยว</td>
+						<td>500 Bath.</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
-<?php echo $footer; ?>
-<script type="text/javascript">
-	// window.print();
+	<div class="col-sm-12">
+		<p class="pull-right">
+			<button class="btn btn-primary btn_print"><span class="fa fa-print"> ปริ๊น</span></button>
+		</p>
+	</div>
+	<?php echo $footer; ?>
+	<script type="text/javascript">
+		$('.btn_print').click(function(){
+		// 	var divToPrint=$("#example");
+		// 	newWin= window.open("");
+		// 	newWin.document.write(divToPrint.html());
+		// // newWin.document.write(divToPrint.outerHTML);
+		// newWin.print();
+		// newWin.close();
+		// window.open($('#example'));
+		window.print($('#example'));
+		window.close();
+	});
 </script>
